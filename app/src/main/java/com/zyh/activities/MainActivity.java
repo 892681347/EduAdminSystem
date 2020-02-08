68e37f190114063ff381bf6bb727b9c6e7073553
892681347 <892681347@qq.com>
2020-02-08 20:35:29 +0800

    实现功能模块的点击切换，去掉滑动切换

--- app/src/main/java/com/zyh/activities/MainActivity.java
+++ app/src/main/java/com/zyh/activities/MainActivity.java
@@ -5,11 +5,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -25,36 +27,33 @@ import com.zyh.fragment.R;
 import com.zyh.fragment.TimetableFragment;
 import com.zyh.utills.Utills;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class MainActivity extends FragmentActivity implements View.OnClickListener{
+public class MainActivity extends FragmentActivity implements OnClickListener {
     private long exitTime = 0;
 
     private TextView topNmae;
 
-    //声明ViewPager
-    private ViewPager mViewPager;
-    //适配器
-    private FragmentPagerAdapter mAdapter;
-    //装载Fragment的集合
-    private List<Fragment> mFragments;
-
-    //四个Tab对应的布局
+    //声明四个Tab的布局文件
     private LinearLayout mTabTimetable;
     private LinearLayout mTabGrade;
     private LinearLayout mTabExam;
     private LinearLayout mTabIndividual;
 
-    //四个Tab对应的ImageButton
+    //声明四个Tab的ImageButton
     private ImageButton mImgTimetable;
     private ImageButton mImgGrade;
     private ImageButton mImgExam;
     private ImageButton mImgIndividual;
+
+    //声明四个Tab分别对应的Fragment
+    private Fragment mFragTimetable;
+    private Fragment mFragGrade;
+    private Fragment mFragExam;
+    private Fragment mFragIndividual;
+
     public LoginBean loginBean;
     public String[] semesters;
 
@@ -67,11 +66,11 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         loginBean = (LoginBean) intent.getSerializableExtra("loginBean");
         Log.d("MainActivity","ActionBegin:Already get loginBean");
         postSemester(loginBean.getData().getToken());
-//        textView.setText(loginBean.toString());
         initViews();//初始化控件
         initEvents();//初始化事件
-        initDatas();//初始化数据
+        selectTab(0);//默认选中第一个Tab
     }
+
     public static void actionStart(Context context, LoginBean loginBean){
         Intent intent = new Intent(context,MainActivity.class);
         intent.putExtra("loginBean", loginBean);
@@ -79,15 +78,129 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         ((Activity)context).finish();
     }
 
+    private void initEvents() {
+        //初始化四个Tab的点击事件
+        mTabTimetable.setOnClickListener(this);
+        mTabGrade.setOnClickListener(this);
+        mTabExam.setOnClickListener(this);
+        mTabIndividual.setOnClickListener(this);
+    }
+
+    private void initViews() {
+        //初始化四个Tab的布局文件
+        mTabTimetable = (LinearLayout) findViewById(R.id.id_tab_weixin);
+        mTabGrade = (LinearLayout) findViewById(R.id.id_tab_frd);
+        mTabExam = (LinearLayout) findViewById(R.id.id_tab_address);
+        mTabIndividual = (LinearLayout) findViewById(R.id.id_tab_setting);
+
+        //初始化四个ImageButton
+        mImgTimetable = (ImageButton) findViewById(R.id.id_tab_weixin_img);
+        mImgGrade = (ImageButton) findViewById(R.id.id_tab_frd_img);
+        mImgExam = (ImageButton) findViewById(R.id.id_tab_address_img);
+        mImgIndividual = (ImageButton) findViewById(R.id.id_tab_setting_img);
+
+        topNmae = (TextView)findViewById(R.id.top_name);
+    }
+
+    //处理Tab的点击事件
     @Override
-    public void onBackPressed() {
-        if ((System.currentTimeMillis() - exitTime) > 2000) {
-            Toast.makeText(getApplicationContext(), "再按一次退出程序",
-                    Toast.LENGTH_SHORT).show();
-            exitTime = System.currentTimeMillis();
-        } else {
-            super.onBackPressed();
+    public void onClick(View v) {
+        //先将四个ImageButton置为灰色
+        resetImgs();
+        switch (v.getId()) {
+            case R.id.id_tab_weixin:
+                selectTab(0);//当点击的是微信的Tab就选中微信的Tab
+                break;
+            case R.id.id_tab_frd:
+                selectTab(1);
+                break;
+            case R.id.id_tab_address:
+                selectTab(2);
+                break;
+            case R.id.id_tab_setting:
+                selectTab(3);
+                break;
         }
+
+    }
+    private void selectTab(int i) {
+        //获取FragmentManager对象
+        FragmentManager manager = getSupportFragmentManager();
+        //获取FragmentTransaction对象
+        FragmentTransaction transaction = manager.beginTransaction();
+        //先隐藏所有的Fragment
+        hideFragments(transaction);
+        switch (i) {
+            //当选中点击的是微信的Tab时
+            case 0:
+                //设置微信的ImageButton为绿色
+                mImgTimetable.setImageResource(R.mipmap.timetable_pressed);
+                topNmae.setText("课程表");
+                //如果微信对应的Fragment没有实例化，则进行实例化，并显示出来
+                if (mFragTimetable == null) {
+                    mFragTimetable = new TimetableFragment();
+                    transaction.add(R.id.id_content, mFragTimetable);
+                } else {
+                    //如果微信对应的Fragment已经实例化，则直接显示出来
+                    transaction.show(mFragTimetable);
+                }
+                break;
+            case 1:
+                mImgGrade.setImageResource(R.mipmap.grade_pressed);
+                topNmae.setText("成绩");
+                if (mFragGrade == null) {
+                    mFragGrade = new GradeFragment();
+                    transaction.add(R.id.id_content, mFragGrade);
+                } else {
+                    transaction.show(mFragGrade);
+                }
+                break;
+            case 2:
+                mImgExam.setImageResource(R.mipmap.exam_pressed);
+                topNmae.setText("考试");
+                if (mFragExam == null) {
+                    mFragExam = new ExamFragment();
+                    transaction.add(R.id.id_content, mFragExam);
+                } else {
+                    transaction.show(mFragExam);
+                }
+                break;
+            case 3:
+                mImgIndividual.setImageResource(R.mipmap.individual_pressed);
+                topNmae.setText("我的");
+                if (mFragIndividual == null) {
+                    mFragIndividual = new IndividualFragment();
+                    transaction.add(R.id.id_content, mFragIndividual);
+                } else {
+                    transaction.show(mFragIndividual);
+                }
+                break;
+        }
+        //不要忘记提交事务
+        transaction.commit();
+    }
+
+    private void hideFragments(FragmentTransaction transaction) {
+        if (mFragTimetable != null) {
+            transaction.hide(mFragTimetable);
+        }
+        if (mFragGrade != null) {
+            transaction.hide(mFragGrade);
+        }
+        if (mFragExam != null) {
+            transaction.hide(mFragExam);
+        }
+        if (mFragIndividual != null) {
+            transaction.hide(mFragIndividual);
+        }
+    }
+
+    //将四个ImageButton置为灰色
+    private void resetImgs() {
+        mImgTimetable.setImageResource(R.mipmap.timetable);
+        mImgGrade.setImageResource(R.mipmap.grade);
+        mImgExam.setImageResource(R.mipmap.exam);
+        mImgIndividual.setImageResource(R.mipmap.individual);
     }
 
     private void postSemester(final String token){
@@ -119,140 +232,15 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
             }
         }).start();
     }
-
-    /*
-     用Fragment和Viewpaper实现滑动和点击效果
-     Begin
-     */
-    private void initDatas() {
-        mFragments = new ArrayList<>();
-        //将四个Fragment加入集合中
-        mFragments.add(new TimetableFragment());
-        mFragments.add(new GradeFragment());
-        mFragments.add(new ExamFragment());
-        mFragments.add(new IndividualFragment());
-
-        //初始化适配器
-        mAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
-            @Override
-            public Fragment getItem(int position) {//从集合中获取对应位置的Fragment
-                return mFragments.get(position);
-            }
-
-            @Override
-            public int getCount() {//获取集合中Fragment的总数
-                return mFragments.size();
-            }
-
-        };
-        //不要忘记设置ViewPager的适配器
-        mViewPager.setAdapter(mAdapter);
-        //设置ViewPager的切换监听
-        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            //页面滚动事件
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            //页面选中事件
-            @Override
-            public void onPageSelected(int position) {
-                //设置position对应的集合中的Fragment
-                mViewPager.setCurrentItem(position);
-                resetImgs();
-                selectTab(position);
-            }
-
-            @Override
-            //页面滚动状态改变事件
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-    }
-
-    private void initEvents() {
-        //设置四个Tab的点击事件
-        mTabTimetable.setOnClickListener(this);
-        mTabGrade.setOnClickListener(this);
-        mTabExam.setOnClickListener(this);
-        mTabIndividual.setOnClickListener(this);
-
-    }
-
-    //初始化控件
-    private void initViews() {
-        mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
-
-        mTabTimetable = (LinearLayout) findViewById(R.id.id_tab_weixin);
-        mTabGrade = (LinearLayout) findViewById(R.id.id_tab_frd);
-        mTabExam = (LinearLayout) findViewById(R.id.id_tab_address);
-        mTabIndividual = (LinearLayout) findViewById(R.id.id_tab_setting);
-
-        mImgTimetable = (ImageButton) findViewById(R.id.id_tab_weixin_img);
-        mImgGrade = (ImageButton) findViewById(R.id.id_tab_frd_img);
-        mImgExam = (ImageButton) findViewById(R.id.id_tab_address_img);
-        mImgIndividual = (ImageButton) findViewById(R.id.id_tab_setting_img);
-
-        topNmae = (TextView)findViewById(R.id.top_name);
-    }
-
     @Override
-    public void onClick(View v) {
-        //先将四个ImageButton置为灰色
-        resetImgs();
-
-        //根据点击的Tab切换不同的页面及设置对应的ImageButton为绿色
-        switch (v.getId()) {
-            case R.id.id_tab_weixin:
-                selectTab(0);
-                break;
-            case R.id.id_tab_frd:
-                selectTab(1);
-                break;
-            case R.id.id_tab_address:
-                selectTab(2);
-                break;
-            case R.id.id_tab_setting:
-                selectTab(3);
-                break;
+    public void onBackPressed() {
+        if ((System.currentTimeMillis() - exitTime) > 2000) {
+            Toast.makeText(getApplicationContext(), "再按一次退出程序",
+                    Toast.LENGTH_SHORT).show();
+            exitTime = System.currentTimeMillis();
+        } else {
+            super.onBackPressed();
         }
     }
-
-    private void selectTab(int i) {
-        //根据点击的Tab设置对应的ImageButton为绿色
-        switch (i) {
-            case 0:
-                mImgTimetable.setImageResource(R.mipmap.timetable_pressed);
-                topNmae.setText("课程表");
-                break;
-            case 1:
-                mImgGrade.setImageResource(R.mipmap.grade_pressed);
-                topNmae.setText("成绩");
-                break;
-            case 2:
-                mImgExam.setImageResource(R.mipmap.exam_pressed);
-                topNmae.setText("考试");
-                break;
-            case 3:
-                mImgIndividual.setImageResource(R.mipmap.individual_pressed);
-                topNmae.setText("我的");
-                break;
-        }
-        //设置当前点击的Tab所对应的页面
-        mViewPager.setCurrentItem(i);
-    }
-
-    //将四个ImageButton设置为灰色
-    private void resetImgs() {
-        mImgTimetable.setImageResource(R.mipmap.timetable);
-        mImgGrade.setImageResource(R.mipmap.grade);
-        mImgExam.setImageResource(R.mipmap.exam);
-        mImgIndividual.setImageResource(R.mipmap.individual);
-    }
-    /*
-     用Fragment和Viewpaper实现滑动和点击效果
-     End
-     */
 }
+
